0080e8e4e3bfb021ad2589588675cd8af662fe2d
sohyunjo <sohyunjo9@gmail.com>
2020-06-02 21:57:20 -0400

    chart added

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'https://covid19.mathdro.id/api';
+const url = "https://covid19.mathdro.id/api";
 
 // async await, easier to read and write
 export const fetchData = async () => {
-    //try will be execuated if fetch is successful 
+    //try will be execuated if fetch is successful
     try {
-        const { data: { confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
+        const {
+            data: { confirmed, recovered, deaths, lastUpdate },
+        } = await axios.get(url);
 
-        return { confirmed, recovered, deaths, lastUpdate};
-        
-    } catch (err) {
+        return { confirmed, recovered, deaths, lastUpdate };
+    } catch (err) {}
+};
 
-    }
-}
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        //convert into an array from object
+        const modifiedData = data.map( dailyData => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+
+        return modifiedData;
+    } catch (err) {}
+};
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+
+import styles from "./Chart.module.css";
 
 const Chart = () => {
+    /* same as
+        state = {
+            dailyData: {}
+        }
+    */
+    const [dailyData, setDailyData] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        };
+        
+        fetchAPI();
+    }, [dailyData]);
+
+    const lineChart = dailyData.length > 0 ? (
+        <Line
+            data={{
+                labels: dailyData.map(({ date }) => date),
+                datasets: [
+                    {
+                        data: dailyData.map(({ confirmed }) => confirmed),
+                        label: "confirmed",
+                        borderColor: "#3333ff",
+                        fill: true,
+                    },
+                    {
+                        data: dailyData.map(({ deaths }) => deaths),
+                        label: "deaths",
+                        borderColor:'red',
+                        backgroundColor: "rgba(255,0,0,0.5)",
+                        fill: true,
+                    }
+                ],
+            }}
+        />
+    ) : null;
+
     return (
-        <h1> Cards</h1>
+        <div className={styles.container}>
+            {lineChart}
+        </div>
     )
-}
+};
 
 export default Chart;
